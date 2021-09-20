cf626fa3d1a8331a25064eb8fbaf76049f4505a8
Adam Zolotarev <adam@prescribefit.com>
2021-09-20 15:03:15 -0400

    Fix max_size to be be actually 1 million instead of 100,000

--- test/mix/tasks/nbx.gen.cache_test.exs
+++ test/mix/tasks/nbx.gen.cache_test.exs
@@ -27,7 +27,7 @@ defmodule Mix.Tasks.Nbx.Gen.CacheTest do
           # GC interval for pushing new generation: 12 hrs
           gc_interval: :timer.hours(12),
           # Max 1 million entries in cache
-          max_size: 1_00_000,
+          max_size: 1_000_000,
           # Max 2 GB of memory
           allocated_memory: 2_000_000_000,
           # GC min timeout: 10 sec
@@ -120,7 +120,7 @@ defmodule Mix.Tasks.Nbx.Gen.CacheTest do
             # GC interval for pushing new generation: 12 hrs
             gc_interval: :timer.hours(12),
             # Max 1 million entries in cache
-            max_size: 1_00_000,
+            max_size: 1_000_000,
             # Max 2 GB of memory
             allocated_memory: 2_000_000_000,
             # GC min timeout: 10 sec
@@ -157,7 +157,7 @@ defmodule Mix.Tasks.Nbx.Gen.CacheTest do
             # GC interval for pushing new generation: 12 hrs
             gc_interval: :timer.hours(12),
             # Max 1 million entries in cache
-            max_size: 1_00_000,
+            max_size: 1_000_000,
             # Max 2 GB of memory
             allocated_memory: 2_000_000_000,
             # GC min timeout: 10 sec
@@ -213,7 +213,7 @@ defmodule Mix.Tasks.Nbx.Gen.CacheTest do
               # GC interval for pushing new generation: 12 hrs
               gc_interval: :timer.hours(12),
               # Max 1 million entries in cache
-              max_size: 1_00_000
+              max_size: 1_000_000
             },
             # Default auto-generated L2 cache (partitioned cache)
             {
@@ -222,7 +222,7 @@ defmodule Mix.Tasks.Nbx.Gen.CacheTest do
                 # GC interval for pushing new generation: 12 hrs
                 gc_interval: :timer.hours(12),
                 # Max 1 million entries in cache
-                max_size: 1_00_000
+                max_size: 1_000_000
               ]
             }
           ]
